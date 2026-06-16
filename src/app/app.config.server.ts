76daaeae554a074cc2910c7117b92cc70fc24c4b
cbracamonte <carlosbracamonte23@gmail.com>
2026-06-16 02:35:07 -0500

    feat: add ProductSSRService for server-side product handling

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -2,10 +2,13 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { PRODUCT_SERVICE } from '@core/tokens';
+import { ProductSSRService } from '@core/services/product-ssr.service';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
+    provideServerRendering(withRoutes(serverRoutes)),
+    { provide: PRODUCT_SERVICE, useClass: ProductSSRService },
   ]
 };
 
